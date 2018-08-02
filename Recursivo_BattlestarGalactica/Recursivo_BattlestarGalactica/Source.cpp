0c5984f722e513f2bd1a3b06db86a993776e80d9
gprossignoli <gprauxiliar@gmail.com>
2018-08-02 18:32:12 +0200

    Ejercicios de recursion

---
+++ Recursivo_BattlestarGalactica/Recursivo_BattlestarGalactica/Source.cpp
@@ -0,0 +1,77 @@
+﻿// Gerardo Parra Rossignoli
+// E45
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+// función que resuelve el problema
+int resolver(std::vector<int> & v, int ini, int end) {
+	int moves = 0;
+	
+	if (v[ini] != ini - 1) {
+		std::swap(v[ini], v[v[ini] - 1]);
+		++moves;
+		return moves;
+	}
+
+	else {
+		int half = (ini + end + 1) / 2;
+		
+		int left = resolver(v, ini, half);
+		int right = resolver(v, half-1, end);
+	
+		return moves + left + right;
+	}
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int nSpaceships, spaceship;
+	std::vector<int> battleFormation;
+	
+	std::cin >> nSpaceships;
+	if (!std::cin)
+		return false;
+	
+	for(int i = 0; i < nSpaceships; ++i) {
+		std::cin >> spaceship;
+		battleFormation.push_back(spaceship);
+	}
+	
+	int necessaryMoves = resolver(battleFormation, 0, battleFormation.size()-1 );
+
+
+	// escribir sol
+
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ Recursivo_UnTrenDeAltaVelocidad/Recursivo_UnTrenDeAltaVelocidad/Source.cpp
@@ -0,0 +1,104 @@
+﻿// Gerardo Parra Rossignoli
+// E45
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <numeric>
+
+
+int calculateAverage(std::vector<int> const & v) {
+	int average = 0;
+	for (int i = 0; i < v.size(); ++i) {
+		average += v[i];
+	}
+	return average / v.size();
+}
+
+bool checkCondition(int const& elem, int const& avg) {
+	int relation = elem - avg;
+	const int halfOfAvg = avg / 2;
+	
+	if (relation > halfOfAvg)
+		return false;
+
+	return true;
+}
+// función que resuelve el problema
+bool resolver(std::vector<int> const& v, int ini, int end, int const& avg) {
+
+	if (ini + 1 == end) {
+		if (!checkCondition(v[ini],avg) || !checkCondition(v[end],avg))
+			return false;
+		
+		return true;
+	}
+
+	else {
+		int half = (ini + end + 1) / 2;
+		
+		bool left = resolver(v, ini, half, avg);
+		if (!left)
+			return false;
+
+		bool right = resolver(v, half, end, avg);
+		if (!right)
+			return false;
+
+		return true;
+	}
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int nElems, elem;
+	std::vector<int> section;
+
+	std::cin >> nElems;
+	if (!std::cin)
+		return false;
+
+	for (int i = 0; i < nElems; ++i) {
+		std::cin >> elem;
+		section.push_back(elem);
+	}
+
+	int average = calculateAverage(section);
+	bool sol = resolver(section, 0 , section.size() - 1, average);
+
+	if (sol)
+		std::cout << "SI\n";
+	else
+		std::cout << "NO\n";
+	// escribir sol
+
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}

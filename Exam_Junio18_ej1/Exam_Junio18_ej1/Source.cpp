98430aaec1c63a040e14a36b71b891ac127f062d
gprossignoli <gprauxiliar@gmail.com>
2018-08-09 12:18:59 +0200

    varios ejercicios de examen

---
+++ Exam_Junio18_ej1/Exam_Junio18_ej1/Source.cpp
@@ -0,0 +1,86 @@
+﻿// Gerardo Parra Rossignoli
+// E45
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+// función que resuelve el problema
+bool resolver(std::vector<int> const& v, const int maxHeight) {
+	int iniSeg, endSeg;
+	bool valid = true;
+	int i = 1;
+	iniSeg = 0;
+
+	/*I --> 1 <= i <= v.size() && valid = forall j,k : 0 <= j <= k < i : v[k] - v[j] <= maxHeight */
+	while (i < v.size()) {
+		if (v[i - 1] >= v[i] && valid)  {
+			endSeg = i - 1;
+			
+			if (v[endSeg] - v[iniSeg] > maxHeight)
+				valid = false;
+
+			iniSeg = i;
+		}
+		/*I --> 1 <= i <= v.size() && valid = exists j,k : 0 <= j < k <= i : v[k] - v[j] > maxHeight*/
+		++i;
+	}
+	//f(i) = v.size() - 1 - i
+	return valid;
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int maxHeight, nHeights, height;
+	std::vector<int> mountain;
+
+	std::cin >> maxHeight;
+	if (!std::cin)
+		return false;
+	std::cin >> nHeights;
+
+	for (int i = 0; i < nHeights; ++i) {
+		std::cin >> height;
+		mountain.push_back(height);
+	}
+
+	bool validRoute = resolver(mountain,maxHeight);
+
+	if (validRoute)
+		std::cout << "apta\n";
+
+	else
+		std::cout << "no apta\n";
+
+
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ Exam_en18_ej2/Exam_en18_ej2/Source.cpp
@@ -0,0 +1,99 @@
+﻿// Gerardo Parra Rossignoli
+// E45
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <stdlib.h>
+
+struct tSolucion {
+	int distance;
+	int value;
+};
+
+// función que resuelve el problema
+tSolucion resolver(std::vector<int> const& v, int const& x,int ini, int end) {
+
+	if (ini + 1 == end) {
+		if (abs(x - v[ini]) < (abs(x - v[end]))) {
+			return { abs(x - v[ini]), v[ini] };
+		}
+
+		if (abs(x - v[ini]) > (abs(x - v[end]))) {
+			return { abs(x - v[end]), v[end] };
+		}
+
+		if (abs(x - v[ini]) == (abs(x - v[end]))) {
+			if (v[ini] < v[end])
+				return { abs(x - v[ini]), v[ini] };
+			else
+				return { abs(x - v[end]), v[end] };
+		}
+	}
+
+	else {
+		int half = (ini + end + 1) / 2;
+		tSolucion left = resolver(v, x, ini, half);
+		tSolucion right = resolver(v, x, half, end);
+
+		if (left.distance < right.distance)
+			return left;
+		if (right.distance < left.distance)
+			return right;
+		if (left.distance == right.distance) {
+			if (left.value < right.value)
+				return left;
+			else
+				return right;
+		}
+	}
+
+	
+
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+	// leer los datos de la entrada
+	int x, nNumbers,number;
+	std::vector<int> v;
+
+	std::cin >> x >> nNumbers;
+	for (int i = 0; i < nNumbers; ++i) {
+		std::cin >> number;
+		v.push_back(number);
+	}
+
+	tSolucion closestNumToX = resolver(v,x,0,v.size()-1);
+	
+	std::cout << closestNumToX.value << '\n';
+
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	int numCasos;
+	std::cin >> numCasos;
+	for (int i = 0; i < numCasos; ++i)
+		resuelveCaso();
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}

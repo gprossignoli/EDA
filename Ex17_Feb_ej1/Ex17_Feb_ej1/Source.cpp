102d7333e4ec2f863412092f0956748528d8ff72
gprossignoli <gprauxiliar@gmail.com>
2018-08-27 21:13:04 +0200

    Examen febrero 2017 primera sesion
    Falta el ejercicio 3

---
+++ Ex17_Feb_ej1/Ex17_Feb_ej1/Source.cpp
@@ -0,0 +1,82 @@
+﻿// Gerardo Parra Rossignoli
+// E45
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+// función que resuelve el problema
+bool checkV(std::vector<int> const& v,const int n,const int d) {
+	int i = 1;
+	bool ok = true; //v is ok if forall int i :: 0 <= i < v.size() then v[i] <= v[i + 1] && (v[i + 1] - v[i]) <= 1 && !exists int k :: (v[i] == k) > d
+	int countK = 1;
+	//invariante: 1 <= i < n && countK >= 1 
+	while (i < n && ok) {
+
+		if (v[i] < v[i - 1] || (v[i] - v[i - 1]) > 1 )
+			ok = false;
+
+		if (v[i] == v[i - 1])
+			++countK;
+		else
+			countK = 1;
+
+		if (countK > d)
+			ok = false;
+		++i;
+	}
+	//invariant: 1 < i <= n && countK <= d 
+	return ok;
+}
+
+
+void resuelveCaso() {
+	int d, n; //d > 0, and n >= 0 
+	std::vector<int> v;
+
+	std::cin >> d >> n;
+
+	if (d > 0 && n > 0) {
+
+		for (int i = 0; i < n; ++i) {
+			int elem;
+			std::cin >> elem;
+			v.push_back(elem);
+		}
+
+		if (checkV(v, n,d))
+			std::cout << "SI\n";
+		else
+			std::cout << "NO\n";
+	}
+	else
+		std::cout << "NO\n";
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	int numCasos;
+	std::cin >> numCasos;
+	for (int i = 0; i < numCasos; ++i)
+		resuelveCaso();
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ Exam17_Feb_ej2/Exam17_Feb_ej2/Source.cpp
@@ -0,0 +1,85 @@
+﻿// Gerardo Parra Rossignoli
+// E45
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+//a vector is ok when for all int i :: 1 <= i < v.size() ==> (v[i] - v[i - 1]) <= 1 
+
+bool isOK(std::vector<int> const& v,const int ini,const int end) { 
+	if (ini == end)
+		return true;
+	
+	else if (ini + 1 == end) {
+		if ((v[end] - v[ini]) <= 1)
+			return true;
+		else
+			return false;
+	}
+	
+	else {
+		int mid = (end + ini) / 2;
+		
+		if (isOK(v, ini, mid))
+			return isOK(v, mid, end);
+		else
+			return false;
+	}
+	
+
+	
+
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+	int n; //tamV >= 0
+	std::vector<int> v; //v is growing, not necessarily strictly increasing
+
+	std::cin >> n;
+	if (n > 0) {
+		for (int i = 0; i < n; ++i) {
+			int elem;
+			std::cin >> elem;
+			v.push_back(elem);
+		}
+
+		if (isOK(v, 0, n - 1))
+			std::cout << "SI\n";
+		else
+			std::cout << "NO\n";
+	}
+	else
+		std::cout << "NO\n";
+
+
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	int numCasos;
+	std::cin >> numCasos;
+	for (int i = 0; i < numCasos; ++i)
+		resuelveCaso();
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}

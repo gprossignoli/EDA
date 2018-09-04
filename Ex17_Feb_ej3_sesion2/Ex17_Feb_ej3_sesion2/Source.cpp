bd9bbb29179866ba0e3c5bd8b4912c79bcf21905
gprossignoli <gprauxiliar@gmail.com>
2018-09-04 14:32:35 +0200

    Ejercicios no resueltos

---
+++ Ex17_Feb_ej3_sesion2/Ex17_Feb_ej3_sesion2/Source.cpp
@@ -0,0 +1,108 @@
+﻿// Gerardo Parra Rossignoli
+// E45
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+void printSol(std::vector<int> const& v){
+	int i = 0;
+	for (i; i < v.size(); ++i) {
+		std::cout << v[i] << " ";
+	}
+	std::cout << v[i] << '\n';
+}
+bool checkValidInsertion(std::vector<int>const&v, const int N, const int elem,std::vector<bool> marcas) {
+	int i = 0;
+
+	if (!marcas[elem])
+		return true;
+
+	if (elem >= (v.size() - 1)) {
+		return false;
+	}
+
+	bool seguir = true;
+	int j = v.size() - 1;
+	while (i < elem && seguir) {
+		if (v[j] == elem)
+			seguir = false;
+		--j;
+		++i;
+	}
+	if (v[j] != elem)
+		return false;
+
+	return true;
+}
+
+void generateSolutions(std::vector<int> & v, const int N, int elem,int & nSol,std::vector<bool> & marcas) {
+	if (v.size() == 2 * N) {
+		printSol(v);
+		++nSol;
+	}
+	else {
+		if (elem <= N) {
+			if (checkValidInsertion(v, N, elem, marcas)) {
+				marcas[elem] = true;
+				v.push_back(elem);
+				generateSolutions(v, N, elem + 1, nSol, marcas);
+				v.pop_back();
+				marcas[elem] = false;
+			}
+			else
+				generateSolutions(v, N, elem + 1, nSol, marcas);
+		}
+		else
+			generateSolutions(v, N, elem - 1, nSol, marcas);
+	}
+
+}
+void resolver(const int N) {
+	std::vector<int> v;
+	std::vector<bool> marcas;
+	for (int i = 0; i < N + 1; ++i)
+		marcas.push_back(false);
+	int solutions = 0;
+
+	generateSolutions(v, N, 1,solutions,marcas);
+	std::cout << "Soluciones: " << solutions << '\n';
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+	int n;
+	std::cin >> n;
+	resolver(n);
+	// escribir sol
+
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	int numCasos;
+	std::cin >> numCasos;
+	for (int i = 0; i < numCasos; ++i)
+		resuelveCaso();
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
---
+++ Iterativo_ParesConLosPares/Iterativo_ParesConLosPares/Source.cpp
@@ -0,0 +1,108 @@
+﻿// Gerardo Parra Rossignoli
+// E45
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+// función que resuelve el problema
+void resolver(std::vector<int> & v,const int sizeV) {
+	int evenToRelocate = 1; 
+	int oddToRelocate = 0;
+
+	if (v.size() == 1) {
+		v.resize(0);
+	}
+
+	else if (v.size() == 2 && v[0] == v[1])
+		v.resize(0);
+
+
+	else {
+		while (evenToRelocate < sizeV && oddToRelocate < sizeV) {
+			if (v[oddToRelocate] % 2 == 0 || v[evenToRelocate] % 2 != 0) {
+				if (v[oddToRelocate] % 2 == 0)
+					oddToRelocate += 2;
+				if (v[evenToRelocate] % 2 != 0)
+					evenToRelocate += 2;
+			}
+			else {
+				std::swap(v[oddToRelocate], v[evenToRelocate]);
+				oddToRelocate += 2;
+				evenToRelocate += 2;
+			}
+		}
+	
+		if (oddToRelocate >= sizeV && evenToRelocate < sizeV) {
+			while (evenToRelocate < sizeV && v[evenToRelocate] % 2 != 0) {
+				evenToRelocate += 2;
+			}
+			v.resize(evenToRelocate);
+		}
+
+		else if (evenToRelocate >= sizeV && oddToRelocate < sizeV) {
+			while (oddToRelocate < sizeV && v[oddToRelocate] % 2 == 0) {
+				oddToRelocate += 2;
+			}
+			v.resize(oddToRelocate);
+		}
+
+		if (oddToRelocate >= sizeV && evenToRelocate >= sizeV && v.size() % 2 != 0)
+			v.resize(v.size() - 1);
+	}
+}
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	int sizeV;
+	std::vector<int> v;
+
+	std::cin >> sizeV;
+	if (!std::cin)
+		return false;
+
+	for (int i = 0; i < sizeV; ++i) {
+		int elem;
+		std::cin >> elem;
+		v.push_back(elem);
+	}
+
+	resolver(v, sizeV);
+	if (v.size() > 0) {
+		int i = 0;
+		for (i; i < v.size() - 1; ++i) {
+			std::cout << v[i] << " ";
+		}
+		std::cout << v[i];
+	}
+	std::cout << '\n';
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso())
+		;
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
